6eb307421e9b34ac8a84ed14a56e1b52138881aa
BallaRaja <ballaraja77@gmai.com>
2025-12-26 01:23:06 +0530

    Backend URL Update

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,4 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "http://10.12.151.127:5000/api/auth";
+  static const String baseUrl =
+      "https://taskmanager-backend-i2cj.onrender.com/api/auth";
   // If real device: use your PC IP instead of 10.0.2.2
 }
