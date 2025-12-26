3f1020d8ec0b40b5a5c3879fa05b0e8dcee6f1aa
BallaRaja <ballaraja77@gmai.com>
2025-12-26 18:53:21 +0530

    Server Side Session Management

--- lib/core/utils/session_manager.dart
+++ lib/core/utils/session_manager.dart
@@ -1,7 +1,19 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SessionManager {
+  static const _tokenKey = "jwt_token";
   static const String _emailKey = "user_email";
+
+  static Future<void> saveSession(String token, String email) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_tokenKey, token);
+    await prefs.setString(_emailKey, email);
+  }
+
+  static Future<String?> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_tokenKey);
+  }
 
   static Future<void> saveEmail(String email) async {
     final prefs = await SharedPreferences.getInstance();
--- lib/features/auth/logic/auth_controller.dart
+++ lib/features/auth/logic/auth_controller.dart
@@ -1,13 +1,17 @@
 import '../data/auth_api.dart';
+import '../data/auth_api.dart';
 
 class AuthController {
-
-  Future<bool> login(String email, String password) async {
-    print("➡️ [AuthController] login()");
-    final result = await AuthApi.login(email, password);
-    print("⬅️ [AuthController] login result: $result");
-    return result;
+  Future<Map<String, dynamic>?> login(String email, String password) {
+    return AuthApi.login(email, password);
   }
+
+  // Future<bool> login(String email, String password) async {
+  //   print("➡️ [AuthController] login()");
+  //   final result = await AuthApi.login(email, password);
+  //   print("⬅️ [AuthController] login result: $result");
+  //   return result;
+  // }
 
   Future<bool> register(String email, String password) async {
     print("➡️ [AuthController] register()");
