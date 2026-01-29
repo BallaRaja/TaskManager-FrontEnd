3b79d70284d29d850231b78a9ad63533bae47539
Kuladeep-Reddy-C <ckuladeepreddy2006@gmail.com>
2026-01-29 22:40:34 +0530

    Clean UP

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
-  static const String baseUrl = "http://0.0.0.0:5000/api/auth";
-  static const String backendUrl = "http://0.0.0.0:5000";
+  static const String baseUrl = "http://10.12.150.244:5000/api/auth";
+  static const String backendUrl = "http://10.12.150.244:5000";
   static const String geminiApiKey = "AIzaSyDO4-DmO23wIf2ipCzqYunqdWC8zkw7InI";
   // "https://taskmanager-backend-i2cj.onrender.com/api/auth";
 }
